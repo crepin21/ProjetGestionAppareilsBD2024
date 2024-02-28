cee7c3968c7e39751865e5e6189afa3a963c9a0f
Crepin21 <fouelefackcrepin@gmail.com>
2024-02-28 11:10:18 -0400

    importation correct

--- src/Organisation/Models/ParametreDB.java
+++ src/Organisation/Models/ParametreDB.java
@@ -2,7 +2,7 @@ package Organisation.Models;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStreamReader;//import
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
